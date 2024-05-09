5a3ab2a6cd17df96be11f2259c45557741f61ea8
Jenovic Mbingazo <djodjombingazo951@gmail.com>
2024-05-09 05:48:13 +0100

    Integrated JavaScript file to manage dynamic behavior.

---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+This JavaScript script manages displaying the list of contacts, grouping them by label, and allowing to add new contacts.
+
+Author: Jenovic MBINGAZO
+Date: From May 7th to May 20th
+
+This script uses only pure JavaScript for a better understanding of the DOM concept.
+
+Features:
+- Display the list of all contacts.
+- Display the list of contacts grouped by label.
+- Add a new contact.
+
+For any questions or improvement suggestions, please contact the author at [djodjombingazo951@gmail.com].
+*/
+
+
+//--------manageDomValues
+function manageDomValues(id) {
+    return document.getElementById(id);
+}
+console.log(manageDomValues("imgInfo").id);
+
+
+
+
+
+
+
+
