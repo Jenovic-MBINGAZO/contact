2730bd04ac79b0a5f37f6e411d6ac3658e298d76
Jenovic MBINGAZO <djodjombingazo951@gmail.com>
2024-05-10 12:12:54 +0100

    delete content

--- index.js
+++ index.js
@@ -23,31 +23,49 @@ function manageDomValues(id) {
     return document.getElementById(id);
 }
 
-const btn = manageDomValues("btn");
+const domValue = manageDomValues("compteur");
+const deleteSpace  = domValue.textContent.trim() ;
+console.log(deleteSpace);
+if (deleteSpace === "Contacts (0)") {
+    const domValue = manageDomValues("mainContent");
+    domValue.remove();
+    const div = document.createElement('div');
+    div.classList = ("main__content");
+    div.id  = ("main__content");
+    const main = manageDomValues("main");
+    main.append(div);
+    const p = document.createElement('p');
+    p.classList = "content--title";
+    p.id = "compteur";
+    p.textContent = "Contacts (0)";
+    div.append(p);
+    const divImg = document.createElement('div');
+    divImg.classList = "content-img";
+    divImg.id = "idContent-img";
+    div.append(divImg);
+    // console.log(div);
+    const img = document.createElement('img');
+    img.src = src="https://ssl.gstatic.com/social/contactsui/images/emptycontacts/emptycontacts_animation_cell1.png";
+    img.alt = "";
+    img.classList = "img-width";
+    img.id = "imgInfo";
+    divImg.append(img);
+    const para = document.createElement('p');
+    para.classList = "class=content-img";
+    para.textContent = "Aucun contact pour l'instant.";
+    para.id = "p";
+    divImg.append(para);
+}
 
-
-btn.addEventListener("DOMContentLoaded", function() {
-    // Sélectionner l'élément à supprimer
-    let elementASupprimer = btn.getElementById("elementASupprimer");
-
-    // Supprimer l'élément si le nombre de contacts est 0 lors du chargement initial
-    if (elementASupprimer && elementASupprimer.textContent.trim() === "Contacts (0)") {
-        elementASupprimer.remove();
-    }
-
-    // Sélectionner le bouton qui déclenche la suppression de l'élément
-    let boutonSuppression = document.getElementById("boutonSuppression");
-
-    // Ajouter un écouteur d'événements de clic au bouton
-    boutonSuppression.addEventListener("click", function(event) {
-        // Supprimer l'élément lorsque le bouton est cliqué
-        if (elementASupprimer) {
-            elementASupprimer.remove();
-        }
-    });
-});
-
-
+/* creation of contact */
+const btn = manageDomValues('btn');
+btn.addEventListener("click",function(event){
+    const pDelete = manageDomValues("compteur");
+    pDelete.remove();
+    const mainContentDelete = manageDomValues("idContent-img");
+    mainContentDelete.remove();
+}
+);
 
 
 
