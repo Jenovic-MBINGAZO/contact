a51b5336e3f140a5abbc0e12f14fa9832b2626f8
Jenovic Mbingazo <djodjombingazo951@gmail.com>
2024-05-12 06:18:15 +0100

    added humbergur menu

--- index.js
+++ index.js
@@ -19,53 +19,83 @@ For any questions or improvement suggestions, please contact the author at [djod
 //
 
 
+/* access to html elements via DOM */
 function manageDomValues(id) {
     return document.getElementById(id);
 }
 
-const domValue = manageDomValues("compteur");
-const deleteSpace  = domValue.textContent.trim() ;
-console.log(deleteSpace);
-if (deleteSpace === "Contacts (0)") {
-    const domValue = manageDomValues("mainContent");
-    domValue.remove();
-    const div = document.createElement('div');
-    div.classList = ("main__content");
-    div.id  = ("main__content");
-    const main = manageDomValues("main");
-    main.append(div);
-    const p = document.createElement('p');
-    p.classList = "content--title";
-    p.id = "compteur";
-    p.textContent = "Contacts (0)";
-    div.append(p);
-    const divImg = document.createElement('div');
-    divImg.classList = "content-img";
-    divImg.id = "idContent-img";
-    div.append(divImg);
-    // console.log(div);
-    const img = document.createElement('img');
-    img.src = src="https://ssl.gstatic.com/social/contactsui/images/emptycontacts/emptycontacts_animation_cell1.png";
-    img.alt = "";
-    img.classList = "img-width";
-    img.id = "imgInfo";
-    divImg.append(img);
-    const para = document.createElement('p');
-    para.classList = "class=content-img";
-    para.textContent = "Aucun contact pour l'instant.";
-    para.id = "p";
-    divImg.append(para);
-}
+//Sending value to the dom
+const search = manageDomValues("search");
+const contacts__search = manageDomValues("contacts__search");
+const search__contacts__boloc = manageDomValues("search__contacts__boloc");
+const checkContactsExist = manageDomValues("checkContactsExist");
+const mainContactsNoExist = manageDomValues("mainContactsNoExist");
+const btnCreateContacts = manageDomValues("btnCreateContacts");
+const hr = manageDomValues("hr");
+// console.log(checkContactsExist);
 
-/* creation of contact */
-const btn = manageDomValues('btn');
-btn.addEventListener("click",function(event){
-    const pDelete = manageDomValues("compteur");
-    pDelete.remove();
-    const mainContentDelete = manageDomValues("idContent-img");
-    mainContentDelete.remove();
+//menu humberger
+const humberger = manageDomValues("humberger") ;
+const main__menu = manageDomValues("main__menu");
+const main__content = manageDomValues("mainContent");
+//console.log(humberger);
+
+/* event listener from dom */
+search.addEventListener("click",function(){
+    // search__contacts__boloc.style.backgroundColor = "red"
+    search__contacts__boloc.classList.add("contacts__search--box-shadow");
+    search.style.backgroundColor = "white";
+   //console.log(search);
 }
 );
+
+//event of btn humberger
+let isMenuOpen = false;
+humberger.addEventListener("click",function(){
+   if (!isMenuOpen) {
+        main__menu.classList.add("main__menu--width");
+        main__content.style.width = "100%";
+        main__content.style.transition = "width 0.5s linear";
+        isMenuOpen = true;
+   }
+   else{
+        main__menu.classList.remove("main__menu--width");
+        main__content.style.width = ""; // Reset width
+        isMenuOpen = false;
+   }
+}
+);
+
+/*  my different functions */
+
+
+//Verification if the contact already exists
+const deleteSpacecheckContactsExist = checkContactsExist.textContent.trim();
+const div = document.createElement("div");
+const divImg = document.createElement('img');
+const p = document.createElement('p');
+div.className = "content__img";
+div.id = "content__img";
+divImg.className = "img__content";
+divImg.alt = "Aucun contact";
+divImg.className = "img__width";
+divImg.src = "https://ssl.gstatic.com/social/contactsui/images/emptycontacts/emptycontacts_animation_cell1.png";
+p.className = "img-description";
+p.textContent = "Aucun contact pour l'instant.";
+
+
+if (deleteSpacecheckContactsExist === "Contacts (0)") {
+    mainContactsNoExist.remove();
+    hr.remove();
+    main__content.append(div);
+    div.append(divImg);
+    setTimeout(function(){
+        divImg.src = "https://ssl.gstatic.com/social/contactsui/images/emptycontacts/emptycontacts_animation_cell3.png";
+    },3000);
+    div.append(p);
+}
+
+
 
 
 
